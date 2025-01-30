9688cec977e540708c503d5b370f51b62717c929
PriyaKshirsagar8 <dnv.intern1@gmail.com>
2025-01-30 17:11:38 +0530

    Added bootstrap + Created neat UI + connected form to local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { EmployeeModel } from './model/Employee';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular-18-CRUD';
+  employeeForm: FormGroup = new FormGroup({});
+  employeeObj: EmployeeModel = new EmployeeModel();
+  employeeList: EmployeeModel[] = [];
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.loadLocalStorageData();
+    } else {
+      console.log('This code is running on the server, localStorage is not available.');
+    }
+    this.createForm();
+  }
+
+  private loadLocalStorageData() {
+    const oldData = localStorage.getItem("EmpData");
+    if (oldData) {
+      this.employeeList = JSON.parse(oldData);
+    }
+  }
+
+  createForm() {
+    this.employeeForm = new FormGroup({
+      empid: new FormControl(this.employeeObj.empId),
+      name: new FormControl(this.employeeObj.name),
+      city: new FormControl(this.employeeObj.city),
+      address: new FormControl(this.employeeObj.address),
+      contactNo: new FormControl(this.employeeObj.contactNo),
+      emailId: new FormControl(this.employeeObj.emailId),
+      pinCode: new FormControl(this.employeeObj.pinCode),
+      state: new FormControl(this.employeeObj.state),
+    });
+  }
+
+  onSave() {
+    if (isPlatformBrowser(this.platformId)) {
+      const oldData = localStorage.getItem("EmpData");
+      if (oldData) {
+        const parseData = JSON.parse(oldData);
+        this.employeeForm.controls['empid'].setValue(parseData.length + 1);
+        this.employeeList.unshift(this.employeeForm.value);
+      } else {
+        this.employeeList.unshift(this.employeeForm.value);
+      }
+      localStorage.setItem("EmpData", JSON.stringify(this.employeeList));
+    }
+  }
 }
